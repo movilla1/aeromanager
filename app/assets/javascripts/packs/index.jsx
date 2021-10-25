7b4ca213d984ec519ebcb5f368c81f8871109fb3
Mario Villarroel <movilla@gmail.com>
2021-10-25 09:26:24 -0300

    Relocate React code

--- app/assets/javascripts/packs/index.jsx
+++ app/assets/javascripts/packs/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ManagerApp from './ManagerApp'
+import ManagerApp from '../ManagerApp'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
