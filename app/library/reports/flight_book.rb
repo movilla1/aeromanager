749c22bde58c2c3bf66de4be21353a0ca4683000
Mario Villarroel <movilla@gmail.com>
2022-02-23 15:34:07 -0300

    Change order call

--- app/library/reports/flight_book.rb
+++ app/library/reports/flight_book.rb
@@ -11,12 +11,12 @@ module Reports
 
     # executes the report data gathering.
     def call
-      query = ::FlightLog.joins(:user, :airplane)
+      query = ::FlightLog.joins(:user, :airplane).order(flight_start: :asc)
       query = query.where("flight_start >= ?", "#{@start_date} 00:00:00") if @start_date.present?
       query = query.where("flight_end <= ?", "#{@end_date} 23:59:59") if @end_date.present?
       query = query.joins(user: [:club_members]).where('club_members.aeroclub_id': @club_id)
       results = []
-      query.order(flight_start: :asc).find_each do |flight_row|
+      query.find_each do |flight_row|
         results << ::FlightBookPresenter.new(flight_row).to_hash
       end
       results
