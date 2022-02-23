7afe42329305816014b1707dab31153dfc16b09f
Mario Villarroel <movilla@gmail.com>
2022-02-23 14:51:14 -0300

    Fix report with passenger count

--- app/presenters/flight_book_presenter.rb
+++ app/presenters/flight_book_presenter.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2021 by Mario O. Villarroel - ElcanSoftware
 class FlightBookPresenter
   attr_reader :airplane, :flight_date, :origin_airport, :destination_airport,
-    :pilot_license_venc, :take_off_hour, :landing_hour, :notes
+    :pilot_license_venc, :take_off_hour, :landing_hour, :notes, :passenger_count
 
   # creates the object and processes data as needed
   def initialize(data_row)
@@ -13,6 +13,7 @@ class FlightBookPresenter
     @take_off_hour = data_row.flight_start.to_formatted_s(:time)
     @landing_hour = data_row.flight_end.to_formatted_s(:time)
     @notes = data_row.notes
+    @passenger_count = data_row.passenger_count
   end
 
   def to_hash
@@ -24,6 +25,7 @@ class FlightBookPresenter
       pilot_license_venc: @pilot_license_venc,
       take_off_hour: @take_off_hour,
       landing_hour: @landing_hour,
+      passenger_count: @passenger_count,
       notes: @notes
     }
   end
